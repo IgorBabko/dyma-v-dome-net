6581f6ac3264e69c9f3aeb0d8cb119d61b36e460
emerge <i.i.babko@gmail.com>
2016-05-27 19:10:25 +0300

    change welcome title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <div class="title">dyma v dome net</div>
             </div>
         </div>
     </body>
