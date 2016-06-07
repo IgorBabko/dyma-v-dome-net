364be570d43d45708444513a2501c0acaa918ee6
emerge <i.i.babko@gmail.com>
2016-06-07 12:32:26 +0300

    add logout button to admin main page

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,7 +1,7 @@
-@extends ('layout')
-
-@section ('content_wrapper')
-    <section style="height: 100vh">
-        <h1>Добро пожаловать в админку!</h1>
-    </section>
+@extends ('layout') @section ('content_wrapper')
+<section style="height: 100vh">
+    <h1 style="text-align: center">Добро пожаловать в админку!<br>
+        <a href="admin/logout" class="btn btn-info">Выйти</a>
+</h1>
+</section>
 @stop
