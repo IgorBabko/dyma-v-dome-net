d33d7b973e83efc094fed6ee108b517dbed24158
emerge <i.i.babko@gmail.com>
2016-06-25 20:32:20 +0300

    add conditions to nav partial

--- resources/views/admin/partials/nav.blade.php
+++ resources/views/admin/partials/nav.blade.php
@@ -1,16 +1,12 @@
 <nav class="collapse navbar-collapse navigation" id="bs-example-navbar-collapse-1" role="navigation">
     <ul class="nav navbar-nav navbar-right">
             <li class="{{ Request::is('admin/chimneys*') ? 'active' : '' }}"><a href="/admin/chimneys" class="">дымоходы</a></li>
+            <li class="{{ Request::is('admin/briquettes*') ? 'active' : '' }}"><a href="/admin/briquettes" class="">брикеты</a></li>
+            <li class="{{ Request::is('admin/boilers*') ? 'active' : '' }}"><a href="/admin/boilers" class="">котлы</a></li>
             <li class="{{ Request::is('admin/articles*') ? 'active' : '' }}"><a href="/admin/articles" class="">статьи</a></li>
             <li class="{{ Request::is('admin/photos*') ? 'active' : '' }}"><a href="/admin/photos" class="">фото</a></li>
             <li class="{{ Request::is('admin/orders*') ? 'active' : '' }}"><a href="/admin/orders" class="">заявки</a></li>
-            <li><a href="/" class="">вернуться на сайт</a></li>
-            <li><a href="/admin/logout" class="">Выйти</a></li>
-            <!--<li class="{{ Request::is('admin/articles') ? 'active' : '' }}"><a href="/chimneys/articles" class="">статьи</a> 
-            </li>
-            <li class="{{ request::is('admin/prices') ? 'active' : '' }}"><a href="/chimneys/prices" class="active">цены</a> 
-            </li>
-            <li class="{{ Request::is('gallery') ? 'active' : '' }}"><a href="/chimneys/gallery" class="">галерея</a> 
-            </li>-->
+            <li><a href="/">вернуться на сайт</a></li>
+            <li><a href="/admin/logout">Выйти</a></li>
     </ul>
 </nav>
--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -2,10 +2,22 @@
     <ul class="nav navbar-nav navbar-right">
             <li><a href="/" class="">главная</a>
             </li>
-            <li class="{{ Request::is('chimneys') ? 'active' : '' }}"><a href="/chimneys" class="">о нас</a> 
+            @if (Request::is('chimneys*'))
+            <li class="{{ Request::is('chimneys') ? 'active' : '' }}"><a href="/chimneys" class="">дымоходы</a> 
             </li>
             <li class="{{ Request::is('chimneys/catalog') ? 'active' : '' }}"><a href="/chimneys/catalog" class="">каталог</a> 
             </li>
+            @elseif (Request::is('briquettes*'))
+            <li class="{{ Request::is('briquettes') ? 'active' : '' }}"><a href="/briquettes" class="">брикеты</a> 
+            </li>
+            <li class="{{ Request::is('briquettes/catalog') ? 'active' : '' }}"><a href="/chimneys/catalog" class="">каталог</a> 
+            </li>
+            @else
+            <li class="{{ Request::is('boilers') ? 'active' : '' }}"><a href="/boilers" class="">котлы</a> 
+            </li>
+            <li class="{{ Request::is('boilers/catalog') ? 'active' : '' }}"><a href="/boilers/catalog" class="">каталог</a> 
+            </li>
+            @endif
             <li class="{{ Request::is('articles*') ? 'active' : '' }}"><a href="/articles" class="">статьи</a> 
             </li>
             <li class="{{ Request::is('prices*') ? 'active' : '' }}"><a href="/chimneys/prices" class="active">цены</a> 
